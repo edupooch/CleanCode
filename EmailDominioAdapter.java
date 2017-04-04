1836273032f855dcded636c3df1dd07c90742649
Eduardo H P Pooch <edupooch@gmail.com>
2017-04-04 10:44:37 -0300

    separação do filtro de resultados em uma outra classe chamada EmailFilter

--- EmailDominioAdapter.java
+++ EmailDominioAdapter.java
@@ -1,4 +1,4 @@
-﻿package br.edu.ufcspa.voltaparacasa.adapters;
+package br.edu.ufcspa.voltaparacasa.adapters;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
@@ -25,27 +25,21 @@ public class EmailDominioAdapter extends ArrayAdapter<String> {
 
     private static final int LAYOUT_DO_BOX = android.R.layout.simple_list_item_1;
 
-    private ArrayList<String> items;
-    private ArrayList<String> itemsClone;
-    private ArrayList<String> sugestoes;
-
-    private Filter nameFilter;
+    private ArrayList<String> itens;
+    private Filter emailFilter; //para filtrar os resultados conforme o usuário digita as letras
 
     public EmailDominioAdapter(Context context, ArrayList<String> dominiosDisponiveis) {
         super(context, LAYOUT_DO_BOX, dominiosDisponiveis);
-        this.items = dominiosDisponiveis;
-        this.itemsClone = new ArrayList<>(dominiosDisponiveis);
-        this.sugestoes = new ArrayList<>();
-        this.nameFilter = criaNameFilter();
+        this.itens = dominiosDisponiveis;
+        this.emailFilter = new EmailFilter(this,itens);
     }
 
     /**
      * Método que cria a view de cada sugestão de domínio
      *
-     * @param position posição no ArrayList de domínios
+     * @param position    posição no ArrayList de domínios
      * @param convertView recurso do adapter para otimizar o carregamento com reutilização de views
-     * @param parent edit text que terá o combo box
-     *
+     * @param parent      edit text que terá o combo box
      * @return view da caixa com a sugestão
      */
     @NonNull
@@ -56,65 +50,16 @@ public class EmailDominioAdapter extends ArrayAdapter<String> {
         if (convertView == null)
             view = inflater.inflate(LAYOUT_DO_BOX, parent, false);
 
-        String dominio = items.get(position);
+        String dominio = itens.get(position);
         TextView textDominio = (TextView) view;
         textDominio.setText(dominio);
 
         return view;
     }
 
+
     @NonNull
     public Filter getFilter() {
-        return nameFilter;
-    }
-
-    private Filter criaNameFilter() {
-        return new Filter() {
-            public String convertResultToString(Object resultValue) {
-                return (String) resultValue;
-            }
-
-            @Override
-            protected FilterResults performFiltering(CharSequence constraint) {
-                if (constraint != null) {
-                    String palabra = constraint.toString();
-                    if (palabra.contains("@")) {
-                        String palabra2 = palabra.substring(palabra.indexOf("@"));
-                        String antesArroba;
-                        try {
-                            antesArroba = palabra.substring(0, palabra.indexOf("@"));
-                        } catch (Exception ex) {
-                            antesArroba = "";
-                        }
-                        sugestoes.clear();
-                        for (String customer : itemsClone) {
-                            if (customer.toLowerCase().startsWith(palabra2.toLowerCase())) {
-                                sugestoes.add(antesArroba + customer);
-                            }
-                        }
-                        FilterResults filterResults = new FilterResults();
-                        filterResults.values = sugestoes;
-                        filterResults.count = sugestoes.size();
-                        return filterResults;
-                    } else {
-                        return new FilterResults();
-                    }
-                } else {
-                    return new FilterResults();
-                }
-            }
-
-            @Override
-            protected void publishResults(CharSequence constraint, FilterResults results) {
-                ArrayList<String> filteredList = (ArrayList<String>) results.values;
-                if (results.count > 0) {
-                    clear();
-                    for (String c : filteredList) {
-                        add(c);
-                    }
-                    notifyDataSetChanged();
-                }
-            }
-        };
+        return emailFilter;
     }
 }
